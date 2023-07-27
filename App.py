ba829ff4ee7ec129ddb42ba6a6c6d25e24714a07
Facundo Elizalde <faculobitoelizalde@gmail.com>
2023-07-27 02:27:15 -0300

    Inicio de sesion base y comentarios

--- App.py
+++ App.py
@@ -1,15 +1,11 @@
 from flask import Flask,request,Response,render_template,redirect,url_for, session
 from http import HTTPStatus
 import json
+from funciones.funciones import nombresPeliculas, imgPeliculas, usersFiles, moviesFiles
 
-usuario_privado=False
+usuario_privado=""
 app = Flask(__name__)
 app.secret_key = 'secretKey1234567890'
-
-with open('json/peliculas.json', encoding='utf-8') as archivo_json1:
-    peliculas = json.load(archivo_json1)
-with open('json/usuarios.json', encoding='utf-8') as file:
-    users = json.load(file)
 
 @app.route("/")
 def retornar():
@@ -18,23 +14,19 @@ def retornar():
 @app.route("/peliculas.html",methods=["GET"])
 @app.route("/peliculas",methods=["GET"])
 def index():
-    lista_nombres_peliculas=[]
-    lista_imagenes_peliculas=[]
-    for i in peliculas[::-1]:
-        if (len(lista_nombres_peliculas)<10) and (i["Nombre"] not in lista_nombres_peliculas):
-            lista_nombres_peliculas.append(i["Nombre"])
-            lista_imagenes_peliculas.append(i["img"])
-
-    return Response (render_template("peliculas.html",
-    nombre_peliculas=lista_nombres_peliculas,
-    imagenes_peliculas=lista_imagenes_peliculas),
-    status = HTTPStatus.OK,)
+  if 'username' in session:
+    user = session['username']
+  else:
+    user = ""
+  return Response (render_template("peliculas.html", user=user, nombre_peliculas=nombresPeliculas(), imagenes_peliculas=imgPeliculas()), status = HTTPStatus.OK)
 
 @app.route("/buscar/<int:info>",methods=["GET"])
 @app.route("/buscar/<info>",methods=["GET"])
 def buscar(info):
     lista_encontradas=[]
+    peliculas = moviesFiles()
     for i in peliculas[::-1]:
+
         for j in i.values():
             if str(info).isnumeric():
                 if ((str(info) in str(j)) and (i not in lista_encontradas)) and (len(lista_encontradas)<10):
@@ -44,39 +36,42 @@ def buscar(info):
                       lista_encontradas.append(i)
 
     return Response (render_template("peliculas.html",
-    nombre_peliculas=[i["Nombre"] for i in lista_encontradas],
-    imagenes_peliculas=[i["img"] for i in lista_encontradas]),
-    status = HTTPStatus.OK,)
+      nombre_peliculas=[i["Nombre"] for i in lista_encontradas],
+      imagenes_peliculas=[i["img"] for i in lista_encontradas]),
+      status = HTTPStatus.OK)
 
-@app.route("/buscar",methods=["POST"])
+@app.route("/buscar", methods=["POST"])
 def buscar_post():
 
     informacion=request.form["info_buscar"]
 
-    return redirect(url_for("buscar", info=informacion,next="edit"),Response=HTTPStatus.OK) 
+    return redirect(url_for("buscar", info=informacion, next="edit"), Response=HTTPStatus.OK) 
 
 @app.route('/login', methods=['GET', 'POST'])
 @app.route('/perfil', methods=['GET', 'POST'])
 def login():
   if 'username' in session:
-    return Response(f'Su usuario ya se encuentra en sesion y es {session["username"]}')
+    return redirect(url_for('index'))
   if request.method == 'POST':
     dataUser = {
       "username": request.form['username'],
       "password": request.form['password']
     }
+    users = usersFiles()
     for user in users:
-      if dataUser["username"] in user["Usuario"] and dataUser["password"] in user["Contrasenia"]:
-        session['username'] = dataUser['username']
-        return Response(f'Usuario logueado correctamente, su usuario es: {session["username"]}')
+      if dataUser["username"] == user["Usuario"] and dataUser["password"] == user["Contrasenia"]:
+        session["username"] = dataUser['username']
+        user = dataUser['username']
       else:
-        return Response("Usuario o contraseña incorrectos, intente de nuevo.")
+        user = ""
+    return Response (render_template("peliculas.html", user=user, nombre_peliculas=nombresPeliculas(), imagenes_peliculas=imgPeliculas()), status = HTTPStatus.OK)
   return render_template('perfil.html')
+
 
 @app.route('/logout')
 def logout():
   session.pop('username', None)
-  return redirect(url_for('peliculas'))
+  return redirect(url_for('index'))
 
 if __name__ == "__main__":
   app.run(debug=True)
---
+++ funciones/funciones.py
@@ -0,0 +1,27 @@
+import json
+
+def moviesFiles():
+  with open('./json/peliculas.json', encoding='utf-8') as archivo_json1:
+    peliculas = json.load(archivo_json1)
+  return peliculas
+
+def usersFiles():
+  with open('./json/usuarios.json', encoding='utf-8') as archivo_json1:
+    users = json.load(archivo_json1)
+  return users
+
+def nombresPeliculas():
+  nombres = []
+  peliculas = moviesFiles()
+  for i in peliculas[::-1]:
+    if (len(nombres)<10) and (i["Nombre"] not in nombres):
+        nombres.append(i["Nombre"])
+  return nombres
+
+def imgPeliculas(): 
+  img = []
+  peliculas = moviesFiles()
+  for i in peliculas[::-1]:
+    if (len(img)<10) and (i["img"] not in img):
+      img.append(i["img"])
+  return img
